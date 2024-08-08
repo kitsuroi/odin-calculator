e600a5ea1721f134f5396c71c22d3f5ca50579be
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-08 11:32:06 +0200

    feat(script:operate): add function operate. calls basic arithmetic based on operator with firstNumber and secondNumber.

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(firstNumber, secondNumber, operator){
+  if (operator === '+') return add(firstNumber, secondNumber);
+  if (operator === '-') return subtract(firstNumber, secondNumber);
+  if (operator === '*') return multiply(firstNumber, secondNumber);
+  if (operator === '/') return divide(firstNumber, secondNumber);
+};
